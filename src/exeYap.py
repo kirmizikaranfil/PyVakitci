a6ca5ff9914dafa6738e056c2fb5faaeaad1537f
Rahman Yazgan <rahmanyazgan@gmail.com>
2020-12-27 23:48:51 +0300

    update

--- src/exeYap.py
+++
@@ -1,6 +0,0 @@
-﻿#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-
-os.system("python setup.py build")
--- src/setup/setup.py
+++ src/setup/setup.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt4 import QtGui, QtCore
 from cx_Freeze import setup, Executable
 
-includes = ["sip","re","atexit","PyQt4"]
+includes = ["atexit","PyQt5"]
 
 includefiles = ["GPLv3_EN.html",
                 "GPLv3_TR.html",
@@ -26,10 +25,10 @@ exe = Executable(
     base = base,
     #targetDir = "dist",
     targetName = "PyVakitci.exe",
-    compress = True,
-    copyDependentFiles = True,
-    appendScriptToExe = True,
-    appendScriptToLibrary = True,
+    #compress = True,
+    #copyDependentFiles = True,
+    #appendScriptToExe = True,
+    #appendScriptToLibrary = True,
     icon = "C:/cami.ico",
     )
 
@@ -37,13 +36,15 @@ setup(
     author = "Rahman Yazgan",
     author_email = 'rahmanyazgan@gmail.com',
     name = "PyVakitci",
-    version = "1.5",
+    version = "1.7",
     description = "Diyanet verilerine göre tüm ülkeler için namaz vakitlerini gösterir. Ezan ve ezan duasını okur.",
     options = {"build_exe": {"includes": includes,
                              "include_files": includefiles,
                              "excludes": excludes,
                              "packages": packages,
-                             "path": path
+                             "path": path,
+                             "zip_include_packages": "*", 
+                             "zip_exclude_packages": ""
                              }
                },
     executables = [exe]
